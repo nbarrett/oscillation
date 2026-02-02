e411b8b6552c5c98a7ae8148ed9830d5a3c0449f
nbarrett <nick.barrett36@me.com>
2026-02-02 00:51:13 +0000

    fix(ui): move error snackbar to top and fix leave game error (ref: #4)

--- src/components/error-snackbar.tsx
+++ src/components/error-snackbar.tsx
@@ -37,7 +37,7 @@ export function ErrorSnackbar() {
   if (errors.length === 0) return null
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2 max-w-md">
+    <div className="fixed top-20 left-1/2 -translate-x-1/2 z-[9999] flex flex-col gap-2 w-full max-w-lg px-4">
       {errors.map((error) => (
         <div
           key={error.id}
